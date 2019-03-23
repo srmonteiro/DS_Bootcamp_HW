2d1843e23e9ebc6de6b472332aa9b38a1eb3da97
srmonteiro <stephen.monteiro@northwestern.edu>
2019-03-22 19:34:52 -0500

    Initial exploratory code entered, getting familiar with datasets

---
+++ Monteiro_wk5_Matplotlib/runningbackup.py
@@ -0,0 +1,38 @@
+# Dependencies and Setup
+%matplotlib inline
+import matplotlib.pyplot as plt
+import pandas as pd
+import numpy as np
+
+# Hide warning messages in notebook
+import warnings
+warnings.filterwarnings('ignore')
+
+# File to Load (Remember to Change These)
+mouse_drug_data_to_load = "data/mouse_drug_data.csv"
+clinical_trial_data_to_load = "data/clinicaltrial_data.csv"
+
+# Jupyter Break___________________________________________________________________________________________________________________________
+
+# Read the Mouse and Drug Data and the Clinical Trial Data
+
+
+
+
+
+
+
+
+# Jupyter Break___________________________________________________________________________________________________________________________
+
+# Combine the data into a single dataset
+
+
+
+
+
+
+
+# Jupyter Break___________________________________________________________________________________________________________________________
+
+# Display the data table for preview
