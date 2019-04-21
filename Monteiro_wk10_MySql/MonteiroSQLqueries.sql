22537eee1589a4bdf87a4c895efad3c7bb503e49
srmonteiro <stephen.monteiro@northwestern.edu>
2019-04-21 13:29:42 -0500

    Final touches, you always spot a typo right after submission

--- Monteiro_wk10_MySql/MonteiroSQLqueries.sql
+++ Monteiro_wk10_MySql/MonteiroSQLqueries.sql
@@ -248,14 +248,14 @@ Where cnt.country = 'Canada';
 SELECT title as 'Family Film Titles'
 FROM film 
 WHERE film_id IN
-  (
+(
   SELECT film_id FROM film_category
   WHERE category_id IN
   (
   SELECT category_id FROM category
   WHERE name = "Family"
   )
-  );
+);
 
 
 # 7e. Display the most frequently rented movies in descending order.
